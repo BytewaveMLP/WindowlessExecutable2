a6b81d242cb468e8fae0f894c052e6e81ced36e9
Eliot Partridge <shortcircuit369@gmail.com>
2017-04-28 21:14:49 -0500

    Add project files.

---
+++ WindowlessExecutable2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowlessExecutable2 {
+	static class Program {
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		
+		[STAThread]
+		static void Main() {
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+
+			using (ProcessIcon pi = new ProcessIcon()) {
+				pi.Display();
+
+				// Make sure the application runs!
+				Application.Run();
+			}
+		}
+	}
+}
---
+++ WindowlessExecutable2/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Windowless Executable 2")]
+[assembly: AssemblyDescription("Do not use Windowless Executable 2 without contacting your IT department. Side effects of Windowless Executable 2 include, but are not limited to: memory loss, CPU slowdown, acute stress, and shitty C code. If you experience more than 50.0 MB of memory loss when using Windowless Executable 2, please contact your local Geek Squad, as this could be a sign of a life-threatening virus. Thank you for choosing Windowless Executable 2.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Windowless Executable Co.")]
+[assembly: AssemblyProduct("Windowless Executable 2")]
+[assembly: AssemblyCopyright("Copyright © Bytewave, 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("80806221-a648-4a46-8b79-1193cd4b167a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.0.*")]
